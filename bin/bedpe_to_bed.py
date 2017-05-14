2d7e3c87205fc237c3ee3dafb0413d1b8f68e934
wckdouglas <wckdouglas@gmail.com>
2017-05-14 16:39:42 -0500

    merge package

---
+++ bin/bedpe_to_bed.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+from tgirt_seq_tools.filter_bed import processFile
+import argparse
+
+def getopt():
+    parser = argparse.ArgumentParser(description='To convert bedpe file to bed file')
+    parser.add_argument('-i',default='-',help='Input bedpe file, must have no unmapped bedpe record (default: -)')
+    parser.add_argument('-o',default='-',help='Output bed file (default: -)')
+    parser.add_argument('--min', default = 10, type=int, help = 'Minimum length of fragment (default = 10)' )
+    parser.add_argument('--max', default = 10000, type=int, help = 'Maximum length of fragment (default = 10000)' )
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = getopt()
+    bed_file = args.i
+    out_file = args.o
+    min_length = args.min
+    max_length = args.max
+    bed_iterator = open(bed_file,'r') if bed_file != '/dev/stdin' and bed_file != '-' else sys.stdin
+    out_handle = open(out_file,'w') if out_file != '-' and out_file != '/dev/stdout' else sys.stdout
+    done = processFile(bed_iterator, min_length, max_length, out_handle)
+
+
+if __name__=='__main__':
+    main()
