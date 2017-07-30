da2c78ac77f42be318b71c2b2d998d2033df184f
wckdouglas <wckdougals@gmail.com>
2017-07-30 02:21:01 -0500

    added test

---
+++ test_script.sh
@@ -0,0 +1,11 @@
+set -e
+
+python --version
+python -c "\
+try:
+    import tgirt_seq_tools
+except ImportError:
+    pass
+"
+python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
+    
