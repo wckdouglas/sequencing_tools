60d32b6e33a7ceff0c46cbae87a5e80dda61251d
Douglas Wu <wckdouglas@gmail.com>
2017-07-31 16:00:42 -0500

    added test script

--- test_script.sh
+++ test_script.sh
@@ -1,7 +1,5 @@
 set -e
 
-python setup.py install
-
 python --version
 python -c "\
 try:
@@ -11,14 +9,14 @@ except ImportError:
 "
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
     
-python bin/bam_to_bed.py -h
-python bin/bam_umi_tag.py -h
-python bin/bedpe_to_bed.py -h
-python bin/clip_fastq.py -h
-python bin/deinterleave_fastq.py -h
-python bin/depth_to_bigwig.py -h
-python bin/filterSoftClip.py -h
-python bin/reduce_multi_reads.py -h
-python bin/split_bam.py -h
-python bin/split_uniq_bam.py -h
-python bin/stranded_base_count.py -h
+python bin/bam_to_bed.py -h &> /dev/null
+python bin/bam_umi_tag.py -h &> /dev/null
+python bin/bedpe_to_bed.py -h &> /dev/null
+python bin/clip_fastq.py -h &> /dev/null
+python bin/deinterleave_fastq.py -h &> /dev/null
+python bin/depth_to_bigwig.py -h &> /dev/null
+python bin/filterSoftClip.py -h &> /dev/null
+python bin/reduce_multi_reads.py -h &> /dev/null
+python bin/split_bam.py -h &> /dev/null
+python bin/split_uniq_bam.py -h &> /dev/null
+python bin/stranded_base_count.py -h &> /dev/null
