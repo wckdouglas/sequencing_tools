91fe8f40ab5bc06dc92c7b7261ede1bf99f0852a
Douglas Wu <wckdouglas@gmail.com>
2017-07-31 15:50:56 -0500

    modiifed requirement

--- test_script.sh
+++ test_script.sh
@@ -9,14 +9,14 @@ except ImportError:
 "
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
     
-bam_to_bed.py
-bam_umi_tag.py
-bedpe_to_bed.py
-clip_fastq.py
-deinterleave_fastq.py
-depth_to_bigwig.py
-filterSoftClip.py
-reduce_multi_reads.py
-split_bam.py
-split_uniq_bam.py
-stranded_base_count.py
+which bam_to_bed.py
+which bam_umi_tag.py
+which bedpe_to_bed.py
+which clip_fastq.py
+which deinterleave_fastq.py
+which depth_to_bigwig.py
+which filterSoftClip.py
+which reduce_multi_reads.py
+which split_bam.py
+which split_uniq_bam.py
+which stranded_base_count.py
