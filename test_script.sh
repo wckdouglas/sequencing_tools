1acdc53bc8c2443eb931a7804ac80c08792e5120
Douglas Wu <wckdouglas@gmail.com>
2017-07-31 11:30:16 -0500

    added test script

--- test_script.sh
+++ test_script.sh
@@ -9,3 +9,14 @@ except ImportError:
 "
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
     
+bam_to_bed.py
+bam_umi_tag.py
+bedpe_to_bed.py
+clip_fastq.py
+deinterleave_fastq.py
+depth_to_bigwig.py
+filterSoftClip.py
+reduce_multi_reads.py
+split_bam.py
+split_uniq_bam.py
+stranded_base_count.py
