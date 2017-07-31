3277763c7f9fbb5b3962e9e7140c5cf05751f70a
Douglas Wu <wckdouglas@gmail.com>
2017-07-31 16:06:05 -0500

    added test bin script

--- test_script.sh
+++ test_script.sh
@@ -10,14 +10,14 @@ except ImportError:
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
     
 
-bin/bam_to_bed.py -h
-bin/bam_umi_tag.py -h
-bin/bedpe_to_bed.py -h
-bin/clip_fastq.py -h
-bin/deinterleave_fastq.py -h
-bin/depth_to_bigwig.py -h
-bin/filterSoftClip.py -h
-bin/reduce_multi_reads.py -h
-bin/split_bam.py -h
-bin/split_uniq_bam.py -h
-bin/stranded_base_count.py -h
+bam_to_bed.py -h
+bam_umi_tag.py -h
+bedpe_to_bed.py -h
+clip_fastq.py -h
+deinterleave_fastq.py -h
+depth_to_bigwig.py -h
+filterSoftClip.py -h
+reduce_multi_reads.py -h
+split_bam.py -h
+split_uniq_bam.py -h
+stranded_base_count.py -h
