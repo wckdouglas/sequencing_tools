90b2f633ad74c208f8d24a83a0152bbdf101a45a
Douglas Wu <wckdouglas@gmail.com>
2017-09-27 14:51:00 -0500

    added TAI

--- sequencing_tools/wobble_codons.py
+++ sequencing_tools/wobble_codons.py
@@ -2,6 +2,7 @@
 
 from sequencing_tools.fastq_tools import  reverse_complement
 from collections import defaultdict
+from scipy.stats.mstats import gmean
 
 class wobble_codon:
     def __init__(self):
@@ -141,7 +142,7 @@ class wobble_codon:
 
 
 class tRNA_adaptation_index:
-    def __init__(self, tRNA_count):
+    def __init__(self, tRNA_count, bacteria=True):
     '''
     adapted from https://github.com/smsaladi/tAI/blob/master/tAI/tAI.py
 
@@ -153,28 +154,41 @@ class tRNA_adaptation_index:
         self.wobble = wobble_codon()
         self.wobble.make_codon_dict()
         self.wobble_dict = self.wobble.codon_dict
-        self.p = {'T': 0.59, 'C': 0.72, 'A': 0.0001, 'G': 0.32}
+        self.tAI_df = None
+
+        self.p = defaultdict(float)
+        for b, s in zip(list('TCAG'), [0.59, 0.72, 0.0001, 0.32]):
+            self.p[b] = s
+
         self.isoleucine_p = 1 - 0.89
 
-        tRNA_availability = defaultdict(int)
-        for codon in wobble_dict.iteritems():
+        self.tRNA_availability = defaultdict(float)
+        for codon in self.wobble_dict.keys():
             codon_prefix = codon[:2]
             wobble_base = codon[2]
 
             if wobble_base == 'T':                  # INN -> NNT, NNC, NNA
-                weights[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['T']*self.tRNA_dict[reverse_complement(codon_prefix+'C')]
+                self.tRNA_availability[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['T']*self.tRNA_dict[reverse_complement(codon_prefix+'C')]
             elif wobble_base == 'C':                # GNN -> NNT, NNC
-                weights[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['C']*self.tRNA_dict[reverse_complement(codon_prefix+'T')]
+                self.tRNA_availability[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['C']*self.tRNA_dict[reverse_complement(codon_prefix+'T')]
             elif wobble_base == 'A':                # TNN -> NNA, NNG
-                weights[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['A']*self.tRNA_dict[reverse_complement(codon_prefix+'T')]
+                self.tRNA_availability[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['A']*self.tRNA_dict[reverse_complement(codon_prefix+'T')]
             elif wobble_base == 'G':                # CNN -> NNG
-                weights[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['G']*self.tRNA_dict[reverse_complement(codon_prefix+'A')]
+                self.tRNA_availability[codon] = self.tRNA_dict[reverse_complement(codon)] + self.p['G']*self.tRNA_dict[reverse_complement(codon_prefix+'A')]
 
-            total_tRNA = sum(tRNA_dict[anticodon] for anticodon in anticodons)
-            tRNA_availability[codon] = total_tRNA
+        if bacteria:
+            tRNA_availability['ATA'] += isoleucine_p*self.tRNA_dict['GAT']
 
 
-   for codon in list(weights.index):
-        base = codon[:2]
+    def tAI(self):
 
-            raise ValueError('Non-standard codon or notation')
+        self.tAI_df = pd.DataFrame({'codon':self.tRNA_availability.keys(), 
+                                    'tRNA_availability': self.tRNA_availability.values()}) \
+            .pipe(lambda d: d[~d.codon.str.contains('ATG|TGA|TAA|TAG')]) \
+            .assign(weight = lambda d: d.tRNA_availability/d.tRNA_availability.sum()) \
+            .assign(weight = lambda d: np.where(d.weight >0, d.weight, gmean(d.weight[d.weight>0])))
+
+    
+
+
+
