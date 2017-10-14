d9cfcf8ab33f467acd2ebe6ebe571aa4136d680d
Douglas Wu <wckdouglas@gmail.com>
2017-10-14 17:41:41 -0500

    added single to multi

--- bin/reduce_multi_reads.py
+++ bin/reduce_multi_reads.py
@@ -4,7 +4,8 @@ import pysam
 import numpy as np
 import sys
 import argparse
-from sequencing_tools.filter_multi import processBam
+from sequencing_tools.filter_multi import process_pair_bam, process_single_bam
+from functools import partial
 
 def getopt():
     parser = argparse.ArgumentParser(description='Process multiply mapped bam and get either shortest insert size or ribosomal reads')
@@ -12,6 +13,7 @@ def getopt():
     parser.add_argument('-o','--outfile', required=True, help = 'Output bam/sam file (use < - > for stdout)' )
     parser.add_argument('-b','--bam_in', action='store_true', help = 'Provide this flag if bam instead of sam is used as input' )
     parser.add_argument('-z','--bam_out', action='store_true', help = 'Provide this flag if bam is needed for output')
+    parser.add_argument('-s','--single_end', action='store_true', help = 'For single end bam files')
     return parser.parse_args()
 
 def main():
@@ -20,6 +22,7 @@ def main():
     out_bam = args.outfile
     bam_in_bool = args.bam_in
     bam_out_bool = args.bam_out
+    processBam = partial(process_pair_bam) if not args.single_end else partial(process_single_bam)
     processBam(in_bam, out_bam, bam_in_bool, bam_out_bool)
     return 0
 
--- sequencing_tools/filter_multi.pyx
+++ sequencing_tools/filter_multi.pyx
@@ -4,6 +4,7 @@ import sys
 from pysam.libcalignmentfile cimport AlignedSegment
 import numpy as np
 from itertools import izip
+import re
 
 
 class read_pairs:
@@ -66,7 +67,72 @@ class read_pairs:
 
     def output_read(self):
         read1_aln, read2_aln = fix_flag(self.out_read1, self.out_read2)
-        return self.out_read1, self.out_read2
+        return read1_aln, read2_aln
+
+cigar_num = re.compile('\d+')
+cigar_str = re.compile('[A-Z]')
+cdef mapped_length(str cigarstring):
+    '''
+    for cigar string, find sum of number of mapped base
+    '''
+    cdef:
+        int cnum
+        int mapped = 0
+        str cstr
+    
+
+    for cnum, cstr in izip(cigar_num.findall(cigarstring), cigar_str.findall(cigarstring)):
+        if cstr == 'M':
+            mapped+=cnum
+    return cnum
+
+class single_read:
+    def __init__(self):
+        self.reads = []
+        self.out_read = None
+        self.read_id = None
+
+    def initiate_group(self, alignment):
+        self.read_id = alignment.query_name
+        self.put_in_group(alignment)
+
+    def put_in_group(self, alignment):
+        self.reads.append(alignment)
+
+    def generate_filter_alingments(self):
+        cdef:
+            AlignedSegment read
+
+        chroms = []
+        isizes = []
+        read_group = []
+        for read in self.reads:
+            isizes.append(mapped_length(read.cigarstring))
+            chroms.append(read.reference_name)
+
+        chroms = np.array(chroms)
+        isizes = np.array(isizes)
+
+        #start filtering
+        size_bool = (isizes == np.min(isizes))
+        ribo_bool = is_ribo_chrom(chroms)
+        regular_chrom_bool = is_regular_chrom(chroms)
+
+        reads = np.array(self.reads)
+
+        if len(isizes[size_bool]) == 1:
+            self.out_read =  reads[size_bool][0]
+        elif len(chroms[ribo_bool]) == 1:
+            self.out_read =  reads[ribo_bool][0]
+        elif len(chroms[regular_chrom_bool]) > 0:
+            self.out_read =  reads[regular_chrom_bool][0]
+        else:
+            self.out_read = reads[0]
+
+    def output_read(self):
+        read_aln = fix_single(self.out_read)
+        return read_aln
+
 
 regular_chroms = range(1,23)
 regular_chroms.extend(list('XY'))
@@ -78,6 +144,12 @@ def is_regular_chrom(chroms):
 
 def is_ribo_chrom(chroms):
     return np.array([True if 'gi' in chrom else False for chrom in chroms])
+
+def fix_single(read):
+    read.flag = 16 if read.is_reverse else 0
+    read.is_supplementary = False
+    read.is_secondary = False
+    return read
 
 def fix_flag(read1, read2):
     read1_flag, read2_flag = 99, 147
@@ -93,7 +165,7 @@ def fix_flag(read1, read2):
     read2.is_secondary = False
     return read1, read2
 
-def processBam(in_bam, out_bam, bam_in_bool, bam_out_bool):
+def process_pair_bam(in_bam, out_bam, bam_in_bool, bam_out_bool):
 
     cdef:
         AlignedSegment alignment, read1_aln, read2_aln
@@ -132,4 +204,43 @@ def processBam(in_bam, out_bam, bam_in_bool, bam_out_bool):
             out_sam.write(read2_aln)
             out_read_count += 1
     sys.stderr.write('Writting %i read pairs\n' %(out_read_count))
+    return 0
+
+def process_single_bam(in_bam, out_bam, bam_in_bool, bam_out_bool):
+
+    cdef:
+        AlignedSegment alignment, read_aln
+        int read_count
+        int out_read_count = 0
+        int read_group_count = 0
+
+    read_flag = 'rb' if bam_in_bool else 'r'
+    write_flag = 'wb' if bam_out_bool else 'w'
+    sys.stderr.write('Start processing bam file: %s\n' %(in_bam))
+    sys.stderr.write('Writing to: %s\n' %(out_bam))
+    with pysam.Samfile(in_bam, read_flag) as in_sam:
+        with pysam.Samfile(out_bam, write_flag, template = in_sam) as out_sam:
+            for read_count, alignment in enumerate(in_sam):
+                if read_group_count == 0:
+                    # initial group for first alignment
+                    read_group = single_read()
+                    read_group.initiate_group(alignment)
+                    read_group_count = 1
+                else:
+                    if alignment.query_name != read_group.read_id:
+                        read_group.generate_filter_alingments()
+                        read_aln = read_group.output_read()
+                        out_sam.write(read_aln)
+                        out_read_count += 1
+                        read_group = single_read()
+                        read_group.initiate_group(alignment)
+                    else:
+                        read_group.put_in_group(alignment)
+            #After all reading whole file, clean out memory and output last alignment group
+            read_group.generate_filter_alingments()
+            read_aln = read_group.output_read()
+
+            out_sam.write(read_aln)
+            out_read_count += 1
+    sys.stderr.write('Writting %i reads\n' %(out_read_count))
     return 0
