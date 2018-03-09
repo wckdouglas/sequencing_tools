6a9158403add0a3ce3c307af6464882454a0083f
Douglas Wu <wckdouglas@gmail.com>
2018-03-09 11:22:17 -0600

    added poisson umi

--- test/test_bed_dedup.py
+++ test/test_bed_dedup.py
@@ -1,11 +1,14 @@
-from sequencing_tools.bam_tools.bed_dedup import *
+from __future__ import print_function
+from sequencing_tools.bam_tools import bed_dedup 
+from sequencing_tools.bam_tools import poisson_umi_tools
+
 
 def test_hamming_barcode():
-    assert(barcode_distance(('ACTG','ACCC')) == 2)
+    assert(bed_dedup.barcode_distance(('ACTG','ACCC')) == 2)
 
 
 def test_fragment_group():
-    fg = fragment_group('chr1','10000', '20000', '-','ACT','')
+    fg = bed_dedup.fragment_group('chr1','10000', '20000', '-','ACT','')
     fg.add_member('ACT','')
     fg.add_member('ACG','')
     assert(not fg.check_fragment('chr1','10000','20000','+'))
@@ -28,4 +31,16 @@ def test_fragment_group():
 
 
     
-    
+
+def test_umi():
+    import os
+    import sys
+    import string
+    f = open(os.devnull, 'w')
+    test_fragment = poisson_umi_tools.fragment('chr1','1000','2000','-', 'AAA')
+    for s in string.ascii_letters:
+        test_fragment.add_fragment(s * 3)
+    assert(test_fragment.output_fragments(umi_nt = 3, out_file = f) == 107)
+    assert(test_fragment.output_fragments(umi_nt = 5, out_file = f) == 53)
+
+
