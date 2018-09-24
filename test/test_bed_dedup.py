86fd10de6df3f784b89ea0f4cd33da5acef6cd89
wckdouglas <wckdouglas@gmail.com>
2018-09-24 12:06:24 -0500

    fixed test

--- test/test_bed_dedup.py
+++ test/test_bed_dedup.py
@@ -35,11 +35,15 @@ def test_umi():
     import os
     import sys
     import string
-    f = open(os.devnull, 'w')
-    test_fragment = poisson_umi_tools.fragment('chr1','1000','2000','-', 'AAA')
+
+    template = 'chr1\t100\t200\t{umi}_1_member\t0\t+'
+    test_f = []
     for s in string.ascii_letters:
-        test_fragment.add_fragment(s * 3)
-    assert(test_fragment.output_fragments(umi_nt = 3, out_file = f) == 107)
-    assert(test_fragment.output_fragments(umi_nt = 5, out_file = f) == 53)
+        test_f += [template.format(umi = (s * 3))]
+
+    outfile = open(os.devnull, 'w')
+    ic, oc = poisson_umi_tools.parse_dedup(test_f, outfile, umi_nt = 3, read_prefix=None)
+    assert(ic == 107 and oc == 52)
+
 
 
