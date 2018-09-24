e2fe26bd0e42181cb6a664b0281f1d33aebdea67
wckdouglas <wckdouglas@gmail.com>
2018-09-24 17:32:08 -0500

    fixed test

--- test/test_bed_dedup.py
+++ test/test_bed_dedup.py
@@ -9,12 +9,13 @@ from itertools import groupby
 def test_fragment_group():
     expected = ['chr1\t10000\t20000\tACG_name\t10000\t+',
                 'chr1\t10000\t20000\tGGG_name\t10000\t+',
-                'chr1\t10000\t20000\tGGG_name\t10000\t+',
                 'chr1\t10000\t20000\tACT_name\t10000\t+',
+                'chr1\t10000\t20000\tGGG_name\t10000\t+',
                 'chr1\t10000\t20000\tACT_name\t10000\t+',
                  'chr1\t10000\t20000\tACT_name\t10000\t+']
 
 
+    member_count = 0
     for coordinates, fragments in groupby(expected, bed_dedup.fragment_coordinates):
         barcode_group = bed_dedup.fragment_group(coordinates, 
                                 fragments, 
@@ -23,8 +24,16 @@ def test_fragment_group():
                                 ct = -1)
         barcode_group.resolve_fragments()
         max_member_count = barcode_group.demultiplexing_barcodes(threshold = 1)
+        uc, mc = barcode_group.print_demultiplexed()
     assert(max_member_count == 4)
+    assert(mc == len(expected))
+    assert(uc == 2)
     barcode_group.unique_barcodes == ['ACG_4_members', 'GGG_2_members']
+
+
+    max_member_count = barcode_group.demultiplexing_barcodes(threshold = 0)
+    assert(max_member_count == 3)
+    assert(barcode_group.unique_barcodes == ['ACG_1_members', 'GGG_2_members', 'ACT_3_members'])
 
 
 def test_umi():
