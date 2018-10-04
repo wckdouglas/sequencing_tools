9fe331c165fe2a72f09efbb0d33fe9d5370a19c8
wckdouglas <wckdouglas@gmail.com>
2018-10-04 00:44:55 -0500

    fixed frag

--- bin/bam_umi_tag.py
+++ bin/bam_umi_tag.py
@@ -10,7 +10,7 @@ def getopt():
     parser.add_argument('-o','--out_bam', default='-', help = 'BAM file output (default: - )')
     parser.add_argument('-t','--tag', default='RX', help = 'Tag id (default: RX )')
     parser.add_argument('-d','--delim', default='_', help = 'Deliminator separating read id and bc (default: _ )')
-    parser.add_argument('-f','--fragment', default=0, 
+    parser.add_argument('-f','--fragment', default='0', 
                         help = 'after splitting read name using {delim}, which' +\
                             'fragment is UMI? can use -1 as last piece (default: 0)')
 
@@ -18,7 +18,7 @@ def getopt():
 
 def main():
     args = getopt()
-    add_umi_tag(args.in_bam, args.out_bam, args.tag, args.delim, args.fragment)
+    add_umi_tag(args.in_bam, args.out_bam, args.tag, args.delim, int(args.fragment))
     return 0
 
 if __name__ == '__main__':
