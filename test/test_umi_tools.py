7375d17379b8c50d67311408d7659937600f5541
Douglas Wu <wckdouglas@utexas.edu>
2018-10-07 09:56:16 -0500

    added nn test

--- test/test_umi_tools.py
+++ test/test_umi_tools.py
@@ -8,14 +8,14 @@ def test_umi_dedup_directional():
     umis = ['ACTGA'] * 10 +\
         ['ACTGG'] *  2 +\
         ['ACTCC'] * 1 +\
-        ['ACCCC'] *  7
+        ['ACCCC'] *  17
 
     umi_counter = Counter(umis)
     out, mc = umi_network.demultiplex_directional(umi_counter)
     assert(sum(int(s.split('_')[1]) for s in out) == len(umis))
     umi_set = set(s.split('_')[0] for s in out)
     assert(umi_set == {'ACCCC','ACTGA'})
-    assert(mc == 12)
+    assert(mc == 18)
 
     umis = ['ACTG','ACCG']
     umi_counter = Counter(umis)
