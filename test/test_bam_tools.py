97a29302fc1656d58858c9a5d4c0504305a16e1d
Douglas Wu <wckdouglas@utexas.edu>
2018-12-25 04:34:04 +0000

    added test

--- test/test_bam_tools.py
+++ test/test_bam_tools.py
@@ -2,6 +2,7 @@ from sequencing_tools.bam_tools import *
 from sequencing_tools.bam_tools.read_cluster import calculate_concensus_base, calculatePosterior
 from pysam import AlignedRead
 import numpy as np
+from scipy.special import logsumexp
 
 
 def artificial_read(flag=163):
@@ -82,13 +83,8 @@ def test_check_primary():
 
 def test_error_correction():
     column_bases = np.array(list('CCAAAAAGT'))
-    column_qualities = np.array(list(')))A--A)A'))
-    base, loglikelihoods = calculate_concensus_base( (column_bases, column_qualities, '') )
-    assert(np.isclose(loglikelihoods, 0.9999999929642414))
-    assert(base == "A")
-
-
-    column_qualities = np.array([ 8,  8,  8, 32, 12, 12, 32,  8, 32])
+    column_qualities_str = np.array(list(')))A--A)A'))
+    column_qualities = np.array(list(map(ord, column_qualities_str))) - 33
     possible_bases = np.unique(column_bases)
     log_posteriors = [calculatePosterior(column_bases, column_qualities, guess_base) for guess_base in possible_bases]
 
@@ -98,3 +94,11 @@ def test_error_correction():
                 -36.420550581755265]
     assert(np.all(np.isclose(log_posteriors, pos_res)))
 
+
+    log_posterior_sum = logsumexp(pos_res)
+    loglik = np.array(log_posteriors) - log_posterior_sum
+
+
+    base, loglikelihood = calculate_concensus_base( (column_bases, column_qualities_str, '') )
+    assert(np.isclose(loglikelihood, np.exp(loglik).max() ) )
+    assert(base == "A")
