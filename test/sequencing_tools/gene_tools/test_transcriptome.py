ea4bc6eb8330b17da294ee44416b1d911a65e063
Douglas Wu <wckdouglas@gmail.com>
2021-09-11 08:51:52 -0400

    refactored test

---
+++ test/sequencing_tools/gene_tools/test_transcriptome.py
@@ -0,0 +1,45 @@
+from sequencing_tools.gene_tools.transcriptome import Transcriptome
+from pathlib import Path
+import pytest
+from sequencing_tools.utils import SeqUtilsError
+from more_itertools import only
+from hypothesis import given, strategies as st
+
+test_data_path = Path(__file__).parent.resolve() / "data"
+
+
+@pytest.fixture(scope="module")
+def transcriptome():
+    return Transcriptome(test_data_path / "test_refflat.txt")
+
+@pytest.fixture(scope="module")
+def transcript(transcriptome):
+    gene = transcriptome.get_gene("WASHC2C")
+    return only(gene.values())
+
+
+def test_Transcriptome_coding_only():
+    transcriptome = Transcriptome(test_data_path / "test_refflat.txt", coding_only=True)
+    assert len(transcriptome.transcript_dict) == 2
+
+
+def test_Transcriptome_bad_gene_name(transcriptome):
+    assert len(transcriptome.transcript_dict) == 2
+    with pytest.raises(SeqUtilsError) as e:
+        transcriptome.get_gene("not_a_gene")
+    assert "not_a_gene not in database" in str(e.value), "Didn't capture bad gene name"
+
+
+def test_transcript(transcript):
+    assert transcript is not None, "Didn't get gene WASHC2C"
+    assert transcript.transcript_length == 4535
+
+
+@given(total_block_size=st.integers(min_value=1, max_value=3000),
+    tstart=st.integers(min_value=1, max_value=1000))
+def test_Transcript_blocks(transcript, total_block_size, tstart):
+    block_size = 0
+    for block_start, block_end in transcript.blocks(tstart, total_block_size+tstart):
+        block_size += (block_end - block_start)
+    assert block_size == total_block_size, "Didn't get correct block size"
+
