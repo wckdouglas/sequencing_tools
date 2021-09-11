566e12aa1cfa443b98aa3cc56977fa5d40e5913c
Douglas Wu <wckdouglas@gmail.com>
2021-09-11 15:22:32 -0400

    refactored stat test

--- test/sequencing_tools/stats_tools/test_regression.py
+++ test/sequencing_tools/stats_tools/test_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from hypothesis import given, strategies as st
 from sequencing_tools.stats_tools.regression import GradientDescent
 
 
@@ -7,11 +8,46 @@ def test_regression():
     # import numpy as np
     np.random.seed(123)
 
-    X = 5 * np.random.rand(100, 2)
-    y = np.matmul(X, np.array([3, 4])) + np.random.randn(100)
+    size = 100
+    beta = np.array([3, 4])
+    X = 5 * np.random.rand(size, 2)
+    y = np.matmul(X, beta) + np.random.randn(size)
     gd = GradientDescent(
         verbose=True, max_iter=100000, lr=1e-2, method="mean", limit=1e-6
     )
     gd.fit(X, y)
-    assert np.abs(gd.B[0] - 3) < 0.1
-    assert np.abs(gd.B[1] - 4) < 0.1
+    assert np.allclose(gd.B, beta, atol=0.1)
+
+
+@given(
+    beta1=st.floats(
+        min_value=0,
+        max_value=20,
+        exclude_min=False,
+        exclude_max=False,
+        allow_infinity=False,
+    ),
+    beta2=st.floats(
+        min_value=-20,
+        max_value=0,
+        exclude_min=False,
+        exclude_max=False,
+        allow_infinity=False,
+    ),
+    beta3=st.floats(
+        min_value=0,
+        max_value=20,
+        exclude_min=False,
+        exclude_max=False,
+        allow_infinity=False,
+    ),
+)
+def test_regression2(beta1, beta2, beta3):
+    beta = np.array([beta1, beta2, beta3])
+    X = 5 * np.random.rand(100, len(beta))  # times 5 for signal-to-noise ratio
+    y = np.matmul(X, beta) + np.random.randn(100)  # the last element is noise
+    gd = GradientDescent(
+        verbose=True, max_iter=100000, lr=1e-2, method="mean", limit=1e-6
+    )
+    gd.fit(X, y)
+    assert np.allclose(gd.B, beta, atol=0.1)
